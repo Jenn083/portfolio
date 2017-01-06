9c6c3b6d32ca8df9cf22e83b7daeb6d31eb65a01
jenn083 <nguyenj1203@gmail.com>
2017-01-06 08:06:23 -0800

    broke day 3 code with handlebars

--- javascript/constructor.js
+++ javascript/constructor.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 var projects = [
   {
     title: 'Rome',
@@ -68,51 +68,56 @@ var projects = [
 
 var articles = [];
 function Projects(options){
-  this.projectUrl=options.projectUrl;
-  this.publishedOn=options.publishedOn;
-  this.body= options.body;
-  this.title=options.title;
-  this.image = options.image;
+  // this.projectUrl=options.projectUrl;
+  // this.publishedOn=options.publishedOn;
+  // this.body= options.body;
+  // this.title=options.title;
+  // this.image = options.image;
+  for(var key in options){
+    this[key] = options[key];
+  }
 }
 
 Projects.prototype.toHtml = function() {
-var $column = $('<div class="col-4"></div>');
-var $title = $('<div class="title"></div>').text(this.title);
-var $image = $('<div class="img"></div>')
-var $picture =$('<img class="image-pic">').attr('src', this.image);
-var $description=$('<div class="description"></div>');
-var $paraDes=$('<p></p>').text(this.body);
-$description.append($paraDes);
-$image.append($picture);
-$image.append($description);
-$column.append($title);
-$column.append($image);
-return $column;
-
+// var $column = $('<div class="col-4"></div>');
+// var $title = $('<div class="title"></div>').text(this.title);
+// var $image = $('<div class="img"></div>')
+// var $picture =$('<img class="image-pic">').attr('src', this.image);
+// var $description=$('<div class="description"></div>');
+// var $paraDes=$('<p></p>').text(this.body);
+// $description.append($paraDes);
+// $image.append($picture);
+// $image.append($description);
+// $column.append($title);
+// $column.append($image);
+// return $column;
+var source = $('article-template').html();
+var templateRender=handlebars.compile(source);
+return templateRender(this);
 };
 
-projects.sort(function(currentObject, nextObject) {
-  return (new Date(nextObject.publishedOn)) - (new Date(currentObject.publishedOn));
-});
+// projects.sort(function(currentObject, nextObject) {
+//   return (new Date(nextObject.publishedOn)) - (new Date(currentObject.publishedOn));
+// });
 
 projects.forEach(function(ele) {
   articles.push(new Projects(ele));
 });
 
-var $rowEl = $('<div class="row"></div>');
-for (var i = 0; i < articles.length; i ++) {
-  $rowEl.append(articles[i].toHtml());
-  console.log(i);
-  console.log(i%3);
-  if ( i >= 0 && i % 3 === 2) {
-    // row = create new row element
-    $('#portfolio').append($rowEl);
-    $rowEl = $('<div class="row"></div>');
-  }
-  // put projects in the row;
+// var $rowEl = $('<div class="row"></div>');
+// for (var i = 0; i < articles.length; i ++) {
+//   $rowEl.append(articles[i].toHtml());
+//   console.log(i);
+//   console.log(i%3);
+//   if ( i >= 0 && i % 3 === 2) {
+//     $('#portfolio').append($rowEl);
+//     $rowEl = $('<div class="row"></div>');
+//   }
+// }
+projects.forEach(function(ele) {
+  articles.push(new Projects(ele));
+});
 
-  // append column element = articles[i].toHtml();
-}
 articles.forEach(function(article) {
   $('#articles').append(article.toHtml());
 });
